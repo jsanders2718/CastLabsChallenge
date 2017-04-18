4f3eab2025b39334da5caaa6abc8fd04610d3bf8
jsanders2718 <jsanders2718@gmail.com>
2017-04-18 07:22:34 -0700

    Add files via upload
    Small improvement to program making analysis more complete

--- MP4 processor/MP4 processor/web File download.vb
+++ MP4 processor/MP4 processor/web File download.vb
@@ -12,9 +12,10 @@ Module Module1
         Dim reply As String = client.DownloadString("http://demo.castlabs.com/tmp/text0.mp4")
 
         ' Write output into local file for visibility while debugging
-        Dim FILE_NAME As String = "D:\My Documents\Cast Labs\test2.txt"
+        Dim FILE_NAME As String = "c:\users\John\Documents\CastLabs\test2.hex"
 
         If System.IO.File.Exists(FILE_NAME) = True Then
+            Beep()
 
             Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
 
@@ -36,47 +37,51 @@ Module Module1
 
         'Get size of moof box - anomalie size field not as expected report but don't act on
 
-        Dim boxSize, moofsize, offset, i As Integer
+        Dim boxSize, moofsize, offset, cumulative As Integer
         Dim Report As String
         Dim mp4Array() As Char = reply.ToCharArray
 
         offset = 0 'define position of first byte to process
 
-        For i = 0 To 20
-            Console.WriteLine(AscW(mp4Array(i)))
-        Next
+        'debug
+        'For i = 0 To 20
+        'Console.WriteLine(AscW(mp4Array(i)))
+        'Next
 
 
         'Crude conversion of byte to number using position to determine exponent.
         'Need to change to loop structure but time running out.
         boxSize = (AscW(mp4Array(offset)) * 256 ^ 3) + (AscW(mp4Array(offset + 1)) * 256 ^ 2) + (AscW(mp4Array(offset + 2)) * 256) + (AscW(mp4Array(offset + 3)))
 
-        moofSize = boxSize   'Save size of moof box to detect end when number of boxes in moof not known
+        moofsize = boxSize   'Save size of moof box to detect end when number of boxes in moof not known
 
         Report = "Found box of type " + Mid(reply, offset + 5, 4) + " and size " & boxSize
         Console.WriteLine(Report)
 
         'move to first box inside moof
-
         offset = offset + 8
 
-        boxSize = (AscW(mp4Array(offset)) * 256 ^ 3) + (AscW(mp4Array(offset + 1)) * 256 ^ 2) + (AscW(mp4Array(offset + 2)) * 256) + (AscW(mp4Array(offset + 3)))
+        While cumulative < moofsize
+            'loop until end of moof box encountered and step to next box
 
-        Report = "Found box of type " + Mid(reply, offset + 5, 4) + " and size " & boxSize
-        'Console.WriteLine(boxSize)
-        Console.WriteLine(Report)
+            boxSize = (AscW(mp4Array(offset)) * 256 ^ 3) + (AscW(mp4Array(offset + 1)) * 256 ^ 2) + (AscW(mp4Array(offset + 2)) * 256) + (AscW(mp4Array(offset + 3)))
 
-        offset = offset + boxSize
+            Report = "Found box of type " + Mid(reply, offset + 5, 4) + " and size " & boxSize
+            'Console.WriteLine(boxSize)
+            Console.WriteLine(Report)
+            If Mid(reply, offset + 5, 4) = " traf" Then  'enter traf box and pull out boxes inside
+                Beep()
+            End If
+            offset = offset + boxSize
+            cumulative = boxSize + 8
 
-        boxSize = (AscW(mp4Array(offset)) * 256 ^ 3) + (AscW(mp4Array(offset + 1)) * 256 ^ 2) + (AscW(mp4Array(offset + 2)) * 256) + (AscW(mp4Array(offset + 3)))
 
-        Report = "Found box of type " + Mid(reply, offset + 5, 4) + " and size " & boxSize
+        End While
+
+        Report = "Content of mdat box " + Mid(reply, offset - boxSize + 9, boxSize)
         'Console.WriteLine(boxSize)
         Console.WriteLine(Report)
         Console.ReadKey()
-
-
-
 
     End Sub
 
